7628e7e1d8dd3ce49a95c4a3ebeb5eaaccb0404e
12yolotl56 <12yolotl56@gmail.com>
2025-06-14 09:01:22 -0600

    updateDB

--- backend/database.py
+++ backend/database.py
@@ -1,4 +1,7 @@
 from pymongo import MongoClient
+import os
 
-client = MongoClient("mongodb://localhost:27017/")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/")
+client = MongoClient(MONGO_URI)
 db = client["mariscos_db"]
+
